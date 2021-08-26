d7226e0086a694a4ec9ced3174e8c9128301df88
asifdastagir2021 <87360355+asifdastagir2021@users.noreply.github.com>
2021-08-26 12:18:26 +0500

    updated code

--- en/signUp/main.js
+++ en/signUp/main.js
@@ -20,15 +20,46 @@ fetchThemeSettings($(".generic-logo-size"));
 $("form#signUp-Form").submit(function() {
     var formData = $("#signUp-Form").serializeArray();
     var model = {};
+	var orgName = model.OrganizationName;
+	var uniqueOrgDataJSON;
+	var obj;
+	var isOrgUnique;
+	
     $.each(formData, function(i, el) {
         model[el.name] = el.value;
     });
     model["Uuid"] = "{{GENERATED_GUID}}"
+	
+	//Fetching JSON Response from OrganizationNameUnique()
+	uniqueOrgDataJSON = $.ajax({
+			url: '/admin/Portal.svc/OrganizationNameUnique()?Name=\'' + model.OrganizationName + '\'',
+			type: 'get',
+			contentType: 'application/json',
+			dataType: 'json',
+			success: function (data) 
+			{
+				return data;
+			},
+        async: false
+			}).responseText;
+			
+	obj = JSON.parse(uniqueOrgDataJSON);
+	
+	//Parsing Parameter to identify if the Organization Name is Unique or Not
+	isOrgUnique = obj.d.result
+	
+	if (!isOrgUnique) {
+        $(".validation.organization").text("Organization name is already taken");
+        $(".validation.organization").show();
+        return false;
+    }
+			
     if (model.cnfemail !== model.Email) {
         $(".validation.signUp").text("Email and confirm email should be same.");
         $(".validation.signUp").show();
         return false;
     }
+		
     delete model.cnfemail;
     model = JSON.stringify(model);
     $.ajax({
