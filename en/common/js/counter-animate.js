5a3675cda8ac2d6a16f5d7f8fee273980d192e59
farhan-ovrlod <farhan.khan@ovrlod.com>
2021-09-02 12:08:06 +0500

    Counter Animation 3rd plugin

---
+++ en/common/js/counter-animate.js
@@ -0,0 +1,24 @@
+/* console.clear();
+
+const spaceHolder = document.querySelector(".space-holder");
+const horizontal = document.querySelector(".horizontal");
+spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`;
+
+function calcDynamicHeight(ref) {
+  const vw = window.innerWidth;
+  const vh = window.innerHeight;
+  const objectWidth = ref.scrollWidth;
+  console.log(ref.scrollWidth);
+  return objectWidth - vw + vh + 150; // 150 is the padding (in pixels) desired on the right side of the .cards container. This can be set to whatever your styles dictate
+}
+
+window.addEventListener("scroll", () => {
+  const sticky = document.querySelector(".sticky");
+  horizontal.style.transform = `translateX(-${sticky.offsetTop}px)`;
+  console.log(`${sticky.offsetTop}`);
+});
+
+window.addEventListener("resize", () => {
+  spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`;
+});
+ */
